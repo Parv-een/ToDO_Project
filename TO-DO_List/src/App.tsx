f4d46e4cb4d62762ce4721f22f5af5cdb1d93599
Parveen Kaur <parveenkaur1045@gmail.com>
2024-08-26 13:08:39 -0600

    first commit

---
+++ TO-DO_List/src/App.tsx
@@ -0,0 +1,10 @@
+import ToDo_Form from "./components/TodoForm";
+
+const App = () => {
+  return (
+    <ToDo_Form />
+  )
+
+}
+
+export default App;
---
+++ TO-DO_List/src/components/TodoForm.tsx
@@ -0,0 +1,26 @@
+export default function ToDo_Form() {
+    return (
+        <>
+            <h1>TO DO LIST</h1>
+            <form>
+
+                <label>Date</label>
+                {/* provide the calender */}
+
+
+                <label>Day</label>
+                <input type="string" placeholder="dayofweek"></input>
+
+                <label>Time</label>
+                {/* provide the time slots  */}
+
+                <label>Things to Do</label>
+                {/* adding list */}
+
+
+
+
+            </form>
+        </>
+    )
+}
